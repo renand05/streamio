1472d16bd2c26479c04c9342320f30e2c54f5b0c
Andres Rengifo <amrengifoh@gmail.com>
2023-10-20 10:12:15 -0500

    feat: add folder structure into auth service

--- projects/auth/auth/main.py
+++ projects/auth/auth/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from .internal import admin
+from .routers import oauth
 
 app = FastAPI()
+
+app.include_router(oauth.router)
+app.include_router(
+    admin.router,
+    prefix="/admin",
+    tags=["admin"],
+    responses={418: {"description": "I'm a teapot"}},
+)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Bigger Applications!"}
